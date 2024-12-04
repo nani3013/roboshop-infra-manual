0a49cd6681323b53127b8520b09e2aca8f59742b
nani3013 <undrunarendra3013@gmai.com>
2024-12-04 19:28:39 +0530

    roboshop

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "roboshop" {
     }
 }
 
-resource "aws_route53_record" {
+resource "aws_route53_record" "www" {
     for_each  = aws_instance.roboshop
     zone_id   = var.zone_id
     name      = each.key == "frontend" ? var.domain_name : "${each.key}.${var.domain_name}"
