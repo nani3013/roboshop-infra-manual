ea8732fb2490bd201ea1f7decb1cb1bd132eb0fd
nani3013 <undrunarendra3013@gmai.com>
2024-12-05 20:19:07 +0530

    roboshop-infra-manual

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "81s-remote-state-dev-2"
-    key    = "expense-vpc"
+    key    = "roboshop-manual"
     region = "us-east-1"
     dynamodb_table = "81s-locking-2"
   }
